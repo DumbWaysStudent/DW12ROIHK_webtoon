477bde15f4a2a644f94bccad0603aef1ae2150fa
unknown <rizky.fadhlillah3@gmail.com>
2019-10-10 12:32:04 +0700

    add profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,18 +1,110 @@
+import { StyleSheet, Dimensions, Text, Share, Image, View, FlatList, SafeAreaView } from 'react-native';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Content, List, ListItem, Thumbnail } from 'native-base';
 
 export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+
+    // handleBack() {
+    //     this.props.navigation.navigate('BottomTabNav')
+    //   }
+    constructor(props) {
+        super(props);
+        this.state = {
+          account:{
+            name: 'Your name',
+            image: 'https://s.kaskus.id/images/2017/02/27/2153697_20170227015800.jpg'
+          },
+  }
+}
+  onClick() {
+    Share.share({
+      message: 'BAM: we\'re helping your business with awesome React Native apps',
+      url: 'http://bam.tech',
+      title: 'Wow, did you see that?'
+    }, {
+      // Android only:
+      dialogTitle: 'Share BAM goodness',
+      // iOS only:
+      excludedActivityTypes: [
+        'com.apple.UIKit.activity.PostToTwitter'
+      ]
+    })
   }
 
   render() {
     return (
-      <View>
-        <Text> Profile </Text>
-      </View>
+      <Container style={styles.Container}>
+        <Header>
+          <Left>
+            <Button  transparent>
+              <Icon name='arrow-back'
+               onPress={()=> alert('back')} />
+            </Button>
+          </Left>
+          <Body>
+            <Title style={styles.title}>Profile</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='create'
+              onPress={()=> alert('Edit')}/>
+            </Button>
+          </Right>
+        </Header>
+
+        <Content style={styles.container}>
+        <View style= {styles.ProfileForm}>
+          <Thumbnail style={styles.ProfileImage}
+            source={{uri: this.state.account.image}}/>
+         
+         <Text style={styles.title}>{this.state.account.name}</Text>
+        </View>
+
+      <SafeAreaView style={styles.form}>
+          <List>
+            <ListItem selected>
+              <Left>
+                <Text style={styles.allText}>My Webtoon Creation</Text>
+              </Left>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </ListItem>
+            <ListItem selected>
+            <Left>
+                <Text style={styles.allText}>Log Out</Text>
+              </Left>
+            </ListItem>
+              
+            </List>
+        </SafeAreaView>
+        </Content>
+      </Container>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width,
+    padding: 4
+  },
+  form: {
+    padding: 5
+  },
+  title: {
+    fontSize: 30,
+  },
+  allText: {
+    fontSize: 20,
+  },
+  ProfileImage:{
+    width: 150, 
+    height: 150,
+    borderRadius: 150/2
+  },  
+  ProfileForm: {
+    marginVertical: 50,
+    alignItems: 'center'
+    
+  },
+})
